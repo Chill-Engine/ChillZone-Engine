18baebb744295b71522ee2dc9cd8d93ef677004c
Nick Salt <46006335+BlueBerrySans365@users.noreply.github.com>
2021-12-06 21:27:19 +0300

    Recoded code ._.
    Now with Python 3.10

--- conf.py
+++
@@ -1,6 +0,0 @@
-
-class configs():
-    client = "619183056636477466"
-    ver = "1.2b"
-    vern = "Beta"
-    headers = {'User-Agent': 'ChillZone v' + ver + " | " + vern + " | " + '(nicksaltfoxu.ml)'}
